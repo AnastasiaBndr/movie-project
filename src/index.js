497dc518679df429264aa1b2bb2307c629947393
AnastasiaBndr <abondarchuk1149@gmail.com>
2024-01-29 21:11:21 +0200

    aaaaaa

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
 
 import './index.css';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
 
 const body = document.querySelector('body');
 body.style.backgroundColor = '#E7ECF2';
@@ -11,8 +15,11 @@ body.style.padding = '40px';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
-      <App />
+      <BrowserRouter basename="movie-project">
+        <Provider store={store}>
+          <App/><Toaster/>
+        </Provider>
     </BrowserRouter>
+    
   </React.StrictMode>
 );
