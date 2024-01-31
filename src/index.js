efbff2755655bd1a81721ffaa710fe4fa2b62e03
AnastasiaBndr <abondarchuk1149@gmail.com>
2024-01-31 17:44:00 +0200

    asas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store';
+// import App from './components/App';
+// import { BrowserRouter } from 'react-router-dom';
+// import store from './redux/store';
 
 import './index.css';
-import { Provider } from 'react-redux';
-import { Toaster } from 'react-hot-toast';
+// import { Provider } from 'react-redux';
+// import { Toaster } from 'react-hot-toast';
 
 
 const body = document.querySelector('body');
@@ -15,11 +15,13 @@ body.style.padding = '40px';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter basename="movie-project">
+
+    <div>Hello</div>
+      {/* <BrowserRouter basename="movie-project">
         <Provider store={store}>
           <App/><Toaster/>
         </Provider>
-    </BrowserRouter>
+    </BrowserRouter> */}
     
   </React.StrictMode>
 );
